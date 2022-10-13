b6f9794244e3ecd7f8d196d88701fc2de240b78f
MariyaKarolia <115708846+MariyaKarolia@users.noreply.github.com>
2022-10-13 13:11:31 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text,View } from “react-native“;
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>mussa</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	        <View style={style.container}>
+	                <Text>mariya</Text>
+<Text>App developer </text>
+                        <StatusBar style=“auto“ />
+               </View>
+       );
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
+		backgroundColor:"#fff",
+		alignItems: "center" ,
+		justifyContent: "center" ,
 	},
-});
+	
