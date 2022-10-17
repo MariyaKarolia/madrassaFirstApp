d89a44122391755a347a141fe4bb0ae95004a73a
MariyaKarolia <115708846+MariyaKarolia@users.noreply.github.com>
2022-10-17 12:59:24 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 import React from 'react'
 import { View, Text }  from "react-native"
 
+const App = () => {
+    return (
+        <View>
+            <Texts>Islamic Knowledge</Text>
+        <View>
+  );
+};
